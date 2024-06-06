a83f36379084b730a479d463446020f27d85d660
KrinjMaster <5opkasuka@gmail.com>
2024-06-06 19:32:29 +0300

    feat: projectiles completed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-//game
 use std::process::exit;
 
 use bevy::{
+    core::FrameCount,
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
@@ -24,12 +24,50 @@ struct Player {
     direction_y: DirectionY,
 }
 
+#[derive(Component)]
+struct Projectile {
+    has_hit: bool,
+}
+
+const SHIP_RADIUS: f32 = 30.;
+const FLIGHT_DISTANCE: f32 = 10.;
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Rusty Invaders".into(),
+                name: Some("Rusty Invaders.app".into()),
+                resolution: (1920., 1080.).into(),
+                focused: true,
+                visible: false,
+                enabled_buttons: bevy::window::EnabledButtons {
+                    maximize: false,
+                    ..Default::default()
+                },
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
-        .add_systems(Update, (player_movement, keyboard_input))
+        .add_systems(
+            Update,
+            (
+                player_movement,
+                keyboard_input,
+                make_visible,
+                projectile_movement,
+            ),
+        )
+        .add_systems(FixedUpdate, shoot_projectile)
+        .insert_resource(Time::<Fixed>::from_seconds(0.75))
         .run();
+}
+
+fn make_visible(mut window: Query<&mut Window>, frames: Res<FrameCount>) {
+    if frames.0 == 10 {
+        window.single_mut().visible = true;
+    }
 }
 
 fn setup(
@@ -39,18 +77,29 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let window = windows.single();
-    let shape = Mesh2dHandle(meshes.add(Circle { radius: 10. }));
+    let shape = Mesh2dHandle(meshes.add(Circle {
+        radius: SHIP_RADIUS,
+    }));
     let color = Color::hex("#ffffff").unwrap_or_else(|err| {
-        println!("{}", err);
+        println!("!! Error: {}", err);
         exit(1);
     });
 
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle {
+        camera: Camera {
+            clear_color: ClearColorConfig::Custom(Color::hex("#000000").unwrap_or_else(|err| {
+                println!("!! Error: {}", err);
+                exit(1);
+            })),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
     commands.spawn((
         MaterialMesh2dBundle {
             mesh: shape,
             material: materials.add(color),
-            transform: Transform::from_xyz(-window.resolution.width() / 2. + 50., 0., 0.),
+            transform: Transform::from_xyz(-window.resolution.width() / 4., 0., 0.),
             ..default()
         },
         Player {
@@ -83,14 +132,14 @@ fn player_movement(mut player: Query<(&mut Transform, &mut Player)>, windows: Qu
 
     match player_info.direction_y {
         DirectionY::Up => {
-            if transform.translation.y + 20. < window.resolution.height() / 2. {
-                transform.translation.y += 5.;
+            if transform.translation.y + SHIP_RADIUS + 10. < window.resolution.height() / 2. {
+                transform.translation.y += FLIGHT_DISTANCE;
                 player_info.direction_y = DirectionY::None;
             };
         }
         DirectionY::Down => {
-            if transform.translation.y - 20. > -window.resolution.height() / 2. {
-                transform.translation.y -= 5.;
+            if transform.translation.y - SHIP_RADIUS - 10. > -window.resolution.height() / 2. {
+                transform.translation.y -= FLIGHT_DISTANCE;
                 player_info.direction_y = DirectionY::None;
             }
         }
@@ -99,17 +148,55 @@ fn player_movement(mut player: Query<(&mut Transform, &mut Player)>, windows: Qu
 
     match player_info.direction_x {
         DirectionX::Left => {
-            if transform.translation.x - 20. > -window.resolution.width() / 2. {
-                transform.translation.x -= 5.;
+            if transform.translation.x - SHIP_RADIUS - 10. > -window.resolution.width() / 2. {
+                transform.translation.x -= FLIGHT_DISTANCE;
             }
             player_info.direction_x = DirectionX::None;
         }
         DirectionX::Right => {
-            if transform.translation.x + 20. < 0. {
-                transform.translation.x += 5.;
+            if transform.translation.x + SHIP_RADIUS + 10. < 0. {
+                transform.translation.x += FLIGHT_DISTANCE;
             }
             player_info.direction_x = DirectionX::None;
         }
         DirectionX::None => (),
+    }
+}
+
+fn shoot_projectile(
+    player: Query<(&Transform, &Player)>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let (transform, _) = player.single();
+
+    let shape = Mesh2dHandle(meshes.add(Rectangle {
+        half_size: Vec2::new(12., 4.),
+    }));
+
+    let color = Color::hex("#ffffff").unwrap_or_else(|err| {
+        println!("!! Error: {}", err);
+        exit(1);
+    });
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: shape,
+            material: materials.add(color),
+            transform: Transform::from_xyz(
+                transform.translation.x,
+                transform.translation.y,
+                transform.translation.z,
+            ),
+            ..default()
+        },
+        Projectile { has_hit: false },
+    ));
+}
+
+fn projectile_movement(mut projectiles: Query<(&mut Transform, &mut Projectile)>) {
+    for (mut transform, _) in projectiles.iter_mut() {
+        transform.translation.x += 10.;
     }
 }
